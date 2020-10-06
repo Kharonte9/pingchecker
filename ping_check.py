b3290ae01cab82745669ccae9bc212ff83364c2a
Knock309 <72338024+Knock309@users.noreply.github.com>
2020-10-06 11:35:35 +0200

    Add files via upload

---
+++ ping_check.py
@@ -0,0 +1,32 @@
+#Autor: eduar766
+#Modificaciones basicas por Knock
+
+from requests import get, exceptions   
+from colorama import Fore, init    
+init()   
+print(Fore.GREEN +"-------------------------------------------------------------")
+print("""
+
+   _____ _               _      _____ _____ _   _  _____ 
+  / ____| |             | |    |  __ \_   _| \ | |/ ____|
+ | |    | |__   ___  ___| | __ | |__) || | |  \| | |  __ 
+ | |    | '_ \ / _ \/ __| |/ / |  ___/ | | | . ` | | |_ |
+ | |____| | | |  __/ (__|   <  | |    _| |_| |\  | |__| |
+  \_____|_| |_|\___|\___|_|\_\ |_|   |_____|_| \_|\_____|
+  
+""")
+
+print("--------------------------------------------------------------")
+
+
+print("¿Que URL quieres comprobar?")
+url = input()
+
+def check_internet_connection():
+    try:
+        get(url , timeout = 3)
+        print('Hay conexion')
+    except exceptions.ConnectionError:
+        print('No hay conexion')
+
+check_internet_connection()
---
+++ readme.txt
@@ -0,0 +1,9 @@
+[ESP]Este script es una modificacion de uno que encontre en internet, 
+lo he modificado dandole un par de detalles insignificantes como el color o el titulo en ASCCI ART
+y tambien la funcion de elegir la URL de la que quieres comprobar el ping, 
+solo lo hize por entretenerme y por practicar y mejorar en Scripting Python.
+
+[ENG] This script is a modification of one I found on the internet,
+I have modified it giving a couple of insignificant details like the color or the title in ASCCI ART
+and also the function of choosing the URL of which you want to check the ping,
+I only did it to entertain myself and to practice and improve in Scripting Python.
